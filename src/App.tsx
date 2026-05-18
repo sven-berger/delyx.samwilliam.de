8153017fa567ab0a33f792e2abc039643f620acb
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-05-18 19:55:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Zähler zum Testen is {count}
         </button>
       </section>
 
