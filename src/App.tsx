97b90a5fb418af4503972b3d50f11e6d69ffd5e6
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-05-18 20:11:02 +0200

    final test

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Testiger Test {count}
+          Count {count}
         </button>
       </section>
 
