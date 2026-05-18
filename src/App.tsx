e344c8a11e69c7807f5630251aaaa3fe07f687f6
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-05-18 20:12:50 +0200

    Test mit allgemeinerer Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
             <li>
               <a href="https://react.dev/" target="_blank">
                 <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
+                Learn LESS HAHAHA
               </a>
             </li>
           </ul>
